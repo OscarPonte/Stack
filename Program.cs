b7ec9a42f047b673d5f74506a68a7c182bc414d1
Oscar <Oscarponteuk@gmail.com>
2020-05-21 12:39:07 +0100

    Adding Projects to GitHub

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(DateTime.Now);
+           // stack.Push(null);
+            stack.Push(8);
+            stack.Push("Test");
+
+            //stack.Clear();
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+           // Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private readonly List<object> _stackList = new List<object>();
+       
+
+        public void Push(object obj)
+        {
+            if (obj == null) throw new InvalidOperationException("Cant add NULL object to the Stack");
+
+            _stackList.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_stackList.Count <= 0) throw new InvalidOperationException("The Stack is empty");
+
+            var last = _stackList.Last();
+            _stackList.RemoveAt(_stackList.Count - 1);
+            return last;
+        }
+
+        public void Clear()
+        {
+            _stackList.Clear();
+        }
+    }
+}
